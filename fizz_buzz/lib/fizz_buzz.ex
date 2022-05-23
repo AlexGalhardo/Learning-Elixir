5edf377a2d1cc085551604867229a55765120ec4
Alex Galhardo <aleexgvieira@gmail.com>
2022-05-23 16:25:57 -0300

    finished aula: convertendo nossa string em uma lista

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -22,6 +22,12 @@ defmodule FizzBuzz do
   end
 
   # pattern matching direto no parametro da função
-  def handle_file_read({:ok, result}), do: result
-  def handle_file_read({:error, reason}), do: reason
+  def handle_file_read({:ok, result}) do
+    # list = String.split(result, ",") # "1,2,3" -> ["1","2","3"]
+    # Enum.map(list, fn number -> String.to_integer(number) end)
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
 end
