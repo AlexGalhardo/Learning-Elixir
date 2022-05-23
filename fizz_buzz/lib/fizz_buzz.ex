3744321a10e57a0a46d90c610a3f8e7079970722
Alex Galhardo <aleexgvieira@gmail.com>
2022-05-23 17:03:11 -0300

    finished fizzbuss project

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -5,10 +5,10 @@ defmodule FizzBuzz do
 
     # {:ok, file} = file = File.read(file_name)
 
-    #case File.read(file_name) do
+    # case File.read(file_name) do
     #  {:ok, result} -> result
     #  {:error, reason} -> reason
-    #end
+    # end
 
     # file = File.read(file_name)
     # handle_file_read(file)
@@ -22,12 +22,32 @@ defmodule FizzBuzz do
   end
 
   # pattern matching direto no parametro da função
-  def handle_file_read({:ok, result}) do
+  defp handle_file_read({:ok, result}) do
     # list = String.split(result, ",") # "1,2,3" -> ["1","2","3"]
     # Enum.map(list, fn number -> String.to_integer(number) end)
-    result
-    |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    result =
+      result
+      |> String.split(",")
+      # |> Enum.map(&String.to_integer/1)
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
   end
-  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
+
+  defp convert_and_evaluate_numbers(element) do
+    # number = String.to_integer(element)
+    # evaluate_numbers(number)
+    element
+    |> String.to_integer()
+    |> evaluate_numbers()
+  end
+
+  # defp -> são funções privadas que o usuário não possui acesso
+
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
 end
